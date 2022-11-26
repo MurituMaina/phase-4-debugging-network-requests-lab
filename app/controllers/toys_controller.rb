2fefadfa49d6e86f601c05e5abc6e900f314a3c2
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-26 14:48:10 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,19 +6,15 @@ class ToysController < ApplicationController
     render json: toys
   end
 
-  def show
-  toy = Toy.find_by(id: params[:id])
-  render json: toy 
-  end
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    # byebug
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+
     render json: toy
   end
 
@@ -29,9 +25,28 @@ class ToysController < ApplicationController
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
+
+# Add a new toy when the toy form is submitted
+# How I debugged:
+# byebug
+# toy = Toys.create(toy_params)
+#internal server error
+# NameError (uninitialized constant ToysController::Toys):
+#toys is uninitialized
+#soln Toys to be Toy
+
+# Update the number of likes for a toy
+# How I debugged:
+#console error alert unhandled rejection
+#fetch got no response(no-content)
+#soln add a render of the update toy
+
+# Donate a toy to Goodwill (and delete it from our database)
+# How I debugged:
+#terminal error ActionController::RoutingError (No route matches [DELETE] "/toys/2"):
+#soln add the :destroy
